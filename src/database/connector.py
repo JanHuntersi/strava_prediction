c331932cea13134aea9c568d069551f5f938510f
= <jan.sernec@gmail.com>
2024-06-05 15:05:08 +0200

    Added get_yesterdays_predictions to db logic

--- src/database/connector.py
+++ src/database/connector.py
@@ -2,7 +2,7 @@ import os
 import src.settings as settings
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 uri = settings.mongodb_connection
@@ -45,16 +45,17 @@ def save_kudos_predictions(collection_name, predictions):
         db = client.get_database(settings.strava_db_name)
         collection = db.get_collection(collection_name)
 
-        # add updated_at timestamp
-        predictions["updated_at"] = datetime.now()
+        #check if existing document already exists
 
-        # update or  insert document
-        collection.update_one(
-            {'kudos_id': predictions['kudos_id']},
-            {"$set": predictions},
-            upsert=True
-        )
-        print(f"{collection_name} saved to mongodb successfully!")
+        existing_document = collection.find_one({"kudos_id": predictions["kudos_id"]})
+        if existing_document:
+            print(f"Document with kudos_id {predictions['kudos_id']} already exists. Updating...")
+
+        else:
+            # add updated_at timestamp
+            predictions["updated_at"] = datetime.now()
+            collection.insert_one(predictions)
+            print(f"{collection_name} saved to mongodb successfully!")
     except Exception as e:
         print(f"Error saving predictions: {e}")
 
@@ -68,3 +69,37 @@ def save_activities_predictions(collection_name, predictions):
         print(f"{collection_name} saved to mongodb successfully!")
     except Exception as e:
         print(f"Error saving predictions: {e}")
+
+def get_all_predictions(collection_name):
+    client = get_client()
+    try:
+        print(f"Trying to get {collection_name} from MongoDB")
+        db = client.get_database(settings.strava_db_name)
+        collection = db.get_collection(collection_name)
+        predictions = collection.find()
+        return predictions
+    except Exception as e:
+        print(f"Error getting predictions: {e}")
+        return None
+
+
+def get_yesterdays_predictions(collection_name):
+    client = get_client()
+    try:
+        print(f"Trying to get {collection_name} from MongoDB")
+        db = client.get_database(settings.strava_db_name)
+        collection = db.get_collection(collection_name)
+        
+        # Get yesterday's date and start and end of day
+        yesterday = datetime.now() - timedelta(days=1)
+        start_of_day = datetime(yesterday.year, yesterday.month, yesterday.day, 0, 0, 0)
+        end_of_day = start_of_day + timedelta(days=1)
+        
+        # Query for documents updated on the previous day
+        predictions = collection.find({
+            "updated_at": {"$gte": start_of_day, "$lt": end_of_day}
+        })
+        return predictions
+    except Exception as e:
+        print(f"Error getting predictions: {e}")
+        return None
