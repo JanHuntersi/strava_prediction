414e0bea97215456c9638555f7fd7e7dd3d088f3
= <jan.sernec@gmail.com>
2024-06-05 15:03:19 +0200

    feature: Evaluate past predictions, workflow

---
+++ src/models/evaluate_predictions.py
@@ -0,0 +1,181 @@
+
+from src.database.connector import get_yesterdays_predictions,get_all_predictions
+from definitions import PATH_TO_KUDOS_FULL_DATASET,PATH_TO_PROCESSED_IS_ACTIVE
+import pandas as pd
+from datetime import datetime
+
+def kudos_find_true_predict(predictions, kudos_full_dataset):
+
+    # Get the kudos_id from the prediction
+    kudos_id = predictions["kudos_id"]
+
+    # Get the true value from the kudos_full_dataset
+    true_kudos = kudos_full_dataset[kudos_full_dataset["id"] == kudos_id]["kudos_count"].values[0]
+
+    # Get the predicted value from the prediction
+    predicted_kudos = predictions["kudos_prediction"]
+
+    print(f"True kudos: {true_kudos}, Predicted kudos: {predicted_kudos}")
+
+    return true_kudos, predicted_kudos
+    
+def evaluate_kudos():
+    print("Trying to evaluate kudos predictions...")
+
+    kudos_df = pd.read_csv(PATH_TO_KUDOS_FULL_DATASET)
+
+    # get predictions from MongoDB
+    predictions = get_yesterdays_predictions("kudos_predictions")
+
+    if predictions is None:
+        print("No predictions yesterday!")
+        return
+
+    # Convert cursor to list to process the predictions
+    predictions = list(predictions)
+
+    if not predictions:
+        print("No predictions yesterday!")
+        return
+
+    print(f"Found {len(predictions)} predictions")
+
+    true_kudos_list = []
+    predicted_kudos_list = []
+
+    # Process each prediction
+    for prediction in predictions:
+        # Perform your evaluation logic here
+        print(f"Evaluating prediction: {prediction}")
+        true, predicted = kudos_find_true_predict(prediction, kudos_df)
+        true_kudos_list.append(true)
+        predicted_kudos_list.append(predicted)
+
+    # Calculate the evaluation metric
+    # For example, Mean Absolute Error (MAE)
+    mae = sum([abs(true - pred) for true, pred in zip(true_kudos_list, predicted_kudos_list)]) / len(true_kudos_list)
+    mse = sum([(true - pred)**2 for true, pred in zip(true_kudos_list, predicted_kudos_list)]) / len(true_kudos_list)
+    print(f"Mean Absolute Error: {mae}") 
+    print(f"Mean Squared Error: {mse}")
+    print("Kudos evaluation done!")
+
+def get_active_values(all_predict_list, df):
+    predictions = []
+    actual_values = []
+
+    for prediction in all_predict_list:
+        date = prediction[1][1]  # Assuming the date is the second element of the tuple
+        is_active_prediction = prediction[0][1]
+
+        # Find the corresponding row in the DataFrame based on the date
+        df_row = df[df['date'] == date]
+
+        if not df_row.empty:
+            actual_value = df_row.iloc[0]['is_active']
+            predictions.append(is_active_prediction)
+            actual_values.append(actual_value)
+
+    return predictions, actual_values
+
+def evaluate_is_active():
+    print("Trying to evaluate is_active predictions...")
+
+    df = pd.read_csv(PATH_TO_PROCESSED_IS_ACTIVE)
+    df = df.tail(48)
+
+    # Convert DataFrame date column to datetime format
+    df['date'] = pd.to_datetime(df['date'])
+
+    # Remove duplicates based on the 'date' column
+    df.drop_duplicates(subset=['date'], inplace=True)
+
+    # get predictions from MongoDB
+    predictions = get_yesterdays_predictions("activities_predictions")
+
+    if predictions is None:
+        print("No predictions yesterday!")
+        return
+
+    # Create an empty list to store all predictions
+    all_predictions = []
+
+    # Iterate through each prediction in predictions
+    for prediction in predictions:
+        pred_list = prediction["predictions"]
+        
+        # Extend the all_predictions list with the pred_list
+        all_predictions.extend(pred_list)
+
+    # Create a DataFrame from all_predictions
+    df_predictions = pd.DataFrame(all_predictions)
+
+    # Drop duplicate rows based on the 'date' column
+    df_predictions.drop_duplicates(subset=['date'], inplace=True)
+
+    if df_predictions.empty:
+        print("No predictions yesterday!")
+        return
+
+    print(f"Found {len(df_predictions)} predictions")
+
+    predicted_values = []
+    actual_values = []
+
+    print(df['date'])
+
+     #date1 = datetime.strptime(date1, '%Y-%m-%d')
+     #remove +00:00 from date
+    df['date'] = pd.to_datetime(df['date'])
+
+     #remove +00:00 from date
+    df['date'] = df['date'].dt.tz_localize(None)
+
+
+    for prediction in df_predictions.to_dict('records'):
+        date = prediction["date"]
+
+        # Ensure that date is a string
+        if isinstance(date, datetime):
+            date_str = date.strftime("%Y-%m-%dT%H:%M:%SZ")
+        else:
+            date_str = date
+
+        # Parse the date string to match the format in DataFrame
+        date_parsed = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
+
+        is_active_prediction = prediction["prediction"]
+
+        # Find the corresponding row in the DataFrame based on the date
+        df_row = df[df['date'] == date_parsed]
+
+        print(date_parsed)
+
+        if not df_row.empty:
+            actual_value = df_row.iloc[0]['is_active']
+            predicted_values.append(is_active_prediction)
+            actual_values.append(actual_value)
+            print(f"Date: {date}, Predicted: {is_active_prediction}, Actual: {actual_value}")
+
+    print("Predicted Values:", predicted_values)
+    print("Actual Values:", actual_values)
+
+    # Calculate the evaluation metric
+    # For example, Accuracy
+    correct_predictions = sum([1 for pred, actual in zip(predicted_values, actual_values) if pred == actual])
+
+    accuracy = correct_predictions / len(actual_values)
+
+    print(f"Accuracy: {accuracy}")
+
+    print("Is_active evaluation done!")
+
+
+def evaluate_predictions():
+
+    evaluate_kudos()
+
+    evaluate_is_active()
+
+
+if __name__ == "__main__":
+    evaluate_predictions()
